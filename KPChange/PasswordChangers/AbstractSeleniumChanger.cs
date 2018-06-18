ad1f3272869fffd16cb7300277e5811fc693ff03
Finn-Thorben Sell <github@finn-thorben.me>
2018-06-18 22:17:25 +0200

    Add new methods for finding object in HTML-DOM

--- KPChange/PasswordChangers/AbstractSeleniumChanger.cs
+++ KPChange/PasswordChangers/AbstractSeleniumChanger.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium;
+﻿using System.Collections.ObjectModel;
+using System.Drawing;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace KPChange.PasswordChangers
@@ -11,12 +13,24 @@ namespace KPChange.PasswordChangers
         public override void OnBegin()
         {
             base.OnBegin();
+            Driver.Manage().Window.Size = new Size(980, 980);
         }
 
         public override void OnEnd()
         {
             base.OnEnd();
-            Driver.Quit();
+            //Driver.Quit();
         }
+        
     }
+
+    public class By : OpenQA.Selenium.By
+    {
+        public static OpenQA.Selenium.By Content(string content) 
+            => XPath("//*[contains(text(), '" + content + "')]");
+
+        public static OpenQA.Selenium.By Attrubute(string key, string value) 
+            => XPath("//*[@" + key + "='" + value + "']");
+    }
+    
 }
